46afeb1e84bb4d2750cec2a68962295e416b6658
Flávia Figueredo <flaviagfigueredo@gmail.com>
2024-09-14 08:04:52 -0300

    feat: adicionar componente Header com estilo inicial

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export function Header() {
+    return (
+        <header className="header">
+            <h2>Meu painel</h2>
+
+            <p>Olá, usuário(a)</p>
+        </header>
+    )
+}
